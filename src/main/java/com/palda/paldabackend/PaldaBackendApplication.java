dd9588e212b4cb1a589de18524fd1a8e4fd83c04
J-I-H-O <jeongjiho0731@gmail.com>
2023-09-17 18:42:04 +0900

    init

---
+++ src/main/java/com/palda/paldabackend/PaldaBackendApplication.java
@@ -0,0 +1,13 @@
+package com.palda.paldabackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaldaBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PaldaBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/palda/paldabackend/PaldaBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.palda.paldabackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaldaBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
